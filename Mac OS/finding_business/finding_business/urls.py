3c858b89b71abda2b1045f34a5716b93fd052eaf
Himanshu Sinha <himanshusin@gmail.com>
2026-09-22 11:22:39 -0400

    feat: initialize project setup files and add root URL redirect

--- Mac OS/finding_business/finding_business/urls.py
+++ Mac OS/finding_business/finding_business/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='team_area51_hackathon/', permanent=False)),
     path('admin/', admin.site.urls),
     path("team_area51_hackathon/", include("the_main_page.urls"))
 ]
